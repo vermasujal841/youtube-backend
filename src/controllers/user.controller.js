b894f4c66745c29d72220eea9d95ba1a34639f80
sujal <vermasujal16@gmail.com>
2024-06-10 05:38:33 +0530

    one more change

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -188,31 +188,45 @@ const refreshAccessToken =asyncHandler(async (req,res)=>{
     if(!incomingRefreshToken){
         throw new ApiError(401,"Unauthorised request")
     }
-    const decodedToken = jwt.verify(incomingRefreshToken,
-        process.env.REFRESH_TOKEN_SECRET
-    )
-    const user =await User.findById(decodedToken?._id)
-    if(!user){
-        throw new ApiError(401,"invalid refresh token")
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET
+        )
+        const user =await User.findById(decodedToken?._id)
+        if(!user){
+            throw new ApiError(401,"invalid refresh token")
+        }
+        if(incomingRefreshToken!==user?.refreshToken){
+            throw new ApiError(401,"Refresh tokem is expired or used")
+        }
+        const options={
+            httpOnly:true,
+            secure:true
+        }
+        await generateAccessAndRefereshTokens(user._id)
+    
+        return res
+        .status(200)
+        .cookie("accessToken",accessToken,options)
+        .cookie("refreshToken",refreshToken,options)
+        .json(
+           new ApiResponse(
+            200,
+            {accessToken,refreshToken: newRefreshToken},
+            "Access token refreshed"
+        )
+        )
+    } catch (error) {
+        throw new ApiError(401,error?.message ||
+            "Invalid refresh token" 
+        )
     }
-    if(incomingRefreshToken!==user?.refreshToken){
-        throw new ApiError(401,"Refresh tokem is expired or used")
-    }
-    const options={
-        httpOnly:true,
-        secure:true
-    }
-    await generateAccessAndRefereshTokens(user._id)
-
-    return res
-    .status(200)
-    .cookie("accessToken",accessToken,options)
-    .cookie("refreshToken",refreshToken,options)
     
 
 })
 export {registerUser,
         loginUser,
-        logoutUser,refreshAccessToken
+        logoutUser,
+        refreshAccessToken
 }
 
