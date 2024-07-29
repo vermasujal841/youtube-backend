645e2839ba6ee7e71a9e1d1cbd690f2f1b5617d1
sujal <vermasujal16@gmail.com>
2024-07-30 03:42:38 +0530

    subs controller complete

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -54,7 +54,7 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
         [
             {
                 $match:{
-                    channel:new mongoose.Types.ObjectId(req.user?._id)
+                    channel:channelId
                 }
             },
             {
@@ -62,16 +62,132 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
                         from: "users",
                         localField: "subscriber",
                         foreignField: "_id",
-                        as: "subscribers"
+                        as: "subscriber",
+                        pipeline:[
+                            {
+                                $lookup:{
+                                    from:"subscribers",
+                                    localField:"_id",
+                                    foreignField:"channel",
+                                    as:"subscribedTo"
+                                },
+                                $addFields:{
+                                    subscribedTosubscriber:{
+                                        $cond:{
+                                            if:{
+                                                $in:[
+                                                    channelId,
+                                                    "$subscribedTo.subscriber"
+                                                ]
+                                            },
+                                            then:"true",
+                                            else:"false" 
+                                        }
+                                    },
+                                    subscriberCount:{
+                                        $size:"$subscribedTosubscriber"
+                                    }
+                                }   
+                            }
+                        ]
+                }
+            },
+            {
+                $unwind:"subscriber"
+            },
+            {
+                $project:{
+                    _id:0,
+                    subscribers:{
+                        _id:1,
+                        username: 1,
+                        fullName: 1,
+                        "avatar.url": 1,
+                        subscribedToSubscriber: 1,
+                        subscribersCount: 1,
+                    }
                 }
             }
+            
         ]
     )
+    if(!subscribers){
+        throw new ApiError(200,"data dose not fetched")
+    }
+    return res
+    .status(200)
+    .json(new ApiResponse(200,subscribers,"subscribers data fetched sucessfully"))
+
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
+    const { subscriberId } = req.params;
+    if(!isValidObjectId(subscriberId)){
+        throw new ApiError(400,"subcriber id does not exist")
+    }
+    const channels=await Subscription.aggregate(
+        [
+            {
+                $match:{
+                    subscriber:subscriberId
+                },
+            },
+            {
+                $lookup:{
+                    from:"users",
+                    localField:"channel",
+                    foreignField:"_id",
+                    as:"userDetails",
+                    pipeline:[
+                    {
+                        $lookup:{
+                            from:"videos",
+                            localField:"_id",
+                            foreignField:"owner",
+                            as:"channelVideos"
+                        },
+                        $addFields:{
+                            latestVideo:{
+                                $last:"$channelVideos"
+                            }
+                        },
+
+                    }
+                    ]
+                }
+            },
+            {
+                $unwind:"$userDetails"
+            },
+            {
+                $project:{
+                    _id: 1,
+                    username: 1,
+                    fullName: 1,
+                    "avatar.url": 1,
+                    latestVideo: {
+                        _id: 1,
+                        "videoFile.url": 1,
+                        "thumbnail.url": 1,
+                        owner: 1,
+                        title: 1,
+                        description: 1,
+                        duration: 1,
+                        createdAt: 1,
+                        views: 1
+                    }
+                }
+            }
+        ]
+    )
+    if(!channels){
+        throw new ApiError(200,"data dose not fetched")
+    }
+    return res
+    .status(200)
+    .json(new ApiResponse(200,channels,"channels data fetched sucessfully"))
+
 })
 
 export {
